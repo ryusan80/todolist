15d46d00706129b34b18923cde8e47df4a9dbf4e
ryusan80 <tintikurin500@gmail.com>
2025-09-20 22:26:55 +0900

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,8 @@
 import "./styles.css";
-import { useState } from "react"
 export default function App() {
-    const [tooText, setTodoText] = useState("");
-    const [incompleteTodos, setIncompleteTOdos] = useState ([
-    ]);
-    const [completeTodos, SetIncompleteTOdos] = useState ([
-    ]);
-
-    const onChengTodoText = (event) => setIncompleteTOdos(event.target.value);
-    
-    const onClickAdd = () => {
-        if (todoText ==="") return;
-        const newTodos = [...incompleteTodos, todoText];
-        setIncompleteTOdos(newTodos);
-        setTodoText("");
-    };
-
-    const onClickDelete = (index) => {
-        const newTodos = [...incompleteTodos];
-        newTodos.splice(index,1);
-        setIncompleteTOdos(newTOdos);
-    };
-
-    const onClickComplete = (index) => {
-         const newIncompleteTodos = [...incompleteTodos];
-        newTodos.splice(index,1);
-
-        const newCompleteTodos = [...completeTOdos, incompleteTOdos[index]];
-        setIncompleteTOdos(newIncompleteTOdos);
-        setCompleteTodos(newCompleteTOdos);
-    };
-
-    const onClickBac= (index) => {
-const newCompleteTodos = [...completeTodos];
-newCompleteTOdos.splice(index,1);
-
-const newCompleteTOdos = [...incompleteTodos, completeTodos[index]];
- setCompleteTodos(newCompleteTOdos);
- setIncompleteTOdos(newIncompleteTOdos);
-    }
-    return (
-        <>
-       <inputTodo/>
-        <div className="input-area">
-            <p className="title">未完了のTODO</p>
-            <ul>
-            {incompleteTodos.map((todo, index) => (
-            
-                <li key={todo}>
-                 <div className="list-row">
-                    <p className="todo-item">{TODO}</p>
-                    <button onClick={() => onClickComplete(index)}>完了</button>
-                    <button onClick={() => onClickDelete(index)}>削除</button>
-                 </div>
-                </li>
-        ))}
-                <li>
-                    <div className="list-row">
-                     <p className="todo-item">TODOです</p>
-                     <button>完了</button>
-                     <button>削除</button>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div className="input-area"> 
-            <p className="title">未完了のTODO</p>
-            <ul>
-                {completeTOdos.map((todo,index) => (
-              <li key={todo}>
-                <div className="List-row">
-                    <p className="Todo-item">TODOでした</p>
-                    <button onClick={() => onClickBack(index)}>戻す</button>
-                    <p className="todo-item">TODOでした</p>
-                    <button>戻す</button>
-                </div>
-              </li>
-             ))}
-            </ul>   
-        </div>
-    </>    
-)}
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,41 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [todoTitle, setTodoTitle] = useState("");
+  const [todoId, setTodoId] = useState(todos.length + 1);
+  const handleAddFormChanges = (e) => {
+    setTodoTitle(e.target.value);
+  };
 
-import { Todo } from "./todo";
+  const handleAddTodo = () => {
+    setTodos([...todos, { id: todoId, title: todoTitle }]);
+    setTodos(todoId + 1);
+    setTodoTitle("");
+  };
 
-const rootElement = document.getElementById("root")
-const root = createRoot(rootElement);
+  const handleDeleteTOdo = (targetTodo) => {
+    setTodos(todo.filter((todo) => todo !== targetTodo));
+  };
 
-root.render(
-    <StrictMode>
-        <todo/>
-    </StrictMode>
-);
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          label="タイトル"
+          value={todoTitle}
+          onchange={handleAddFormChanges}
+        />
+      </div>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span>{todo.title}</span>
+            <button onClick={() => handleDeleteTOdo(todo)}>削除</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
