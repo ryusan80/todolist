422cd4d55eae01e7c25e2d5561100016ba90ab7e
ryusan80 <tintikurin500@gmail.com>
2025-11-05 22:40:44 +0900

    更新機能

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ export const App: React.FC = () => {
   };
 
   // 未完リストから削除
-  const onClickDelete = (index: number) => {
+  const onClickDeleteIncomplete = (index: number) => {
     setIncompleteTodos((prev) => prev.filter((_, i) => i !== index));
   };
 
@@ -38,42 +38,62 @@ export const App: React.FC = () => {
     setCompleteTodos((prev) => prev.filter((_, i) => i !== index));
     setIncompleteTodos((prev) => [...prev, todo]);
   };
+  // 完了リストで削除
+  const onClickDeleteComplete = (index: number) => {
+    setCompleteTodos((prev) => prev.filter((_, i) => i !== index));
+  };
 
   return (
     <>
+      <header className="flex-justify-between mx-auto container  text-right">
+        <div className="self-auto font-serif italic justify-self-end ml-auto space-x-6">
+          <button className="flex-16 bg-emerald-400">signup</button>
+          <button className="flex-16 bg-emerald-400">signin</button>
+          <button className="flex-16 bg-emerald-400">logout</button>
+        </div>
+      </header>
       <form onSubmit={handleSubmit}>
+        <h1 className="font-serif italic flex text-center">TODOLIST</h1>
         <input
           type="text"
           placeholder="TODOを入力"
           value={todoText}
           onChange={handleChange}
         />
-        <button type="submit">追加</button>
+        <button type="submit" className=" bg-green-100 ">
+          追加
+        </button>
       </form>
 
-      <div>
-        <p className="title">未完のTODO</p>
+      <div className="flex w-1/2 border border-gray-300 rounded p-4 mr-4">
+        <p className="text-lg font-semibold mb-2">未完のTODO</p>
+        
         <ul>
           {incompleteTodos.map((todo, index) => (
             <li key={index}>
               <div className="list-row">
                 <p className="todo-item">{todo}</p>
                 <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
+                <button onClick={() => onClickDeleteIncomplete(index)}>
+                  削除
+                </button>
               </div>
             </li>
           ))}
         </ul>
       </div>
 
-      <div>
-        <p className="title">完了のTODO</p>
+      <div className="w-1/2 border border-gray-300 rounded p-4 mr-4">
+      <p className="text-lg font-semibold mb-2 ">完了のTODO</p>
         <ul>
           {completeTodos.map((todo, index) => (
             <li key={index}>
               <div className="list-row">
                 <p className="todo-item">{todo}</p>
                 <button onClick={() => onClickReturn(index)}>戻す</button>
+                <button onClick={() => onClickDeleteComplete(index)}>
+                  削除
+                </button>
               </div>
             </li>
           ))}
