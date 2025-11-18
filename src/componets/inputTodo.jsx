92d45d0bae22c7b4a5ff83f5930f6e0201cf612e
ryusan80 <tintikurin500@gmail.com>
2025-11-18 19:54:22 +0900

    todolist更新

--- src/componets/inputTodo.jsx
+++
@@ -1,8 +0,0 @@
-export const InputTOdo = () => {
-    return (
- <div className="input-area">
-            <input placeholder="TODOを入力" value={todoText} onCheng={("")} />
-            <button onClick={onClickAdd}>追加</button>
-        </div>
-    );
-};
